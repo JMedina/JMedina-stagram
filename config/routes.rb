1a542bbc088d295f8e912a91b262aaf1cf3699f1
= <jamedina@hmc.edu>
2017-02-15 17:07:28 -0800

    add rest of folders

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+#Rails.application.routes.draw do
+ # get 'posts/new'
+
+ # get 'posts/index'
+
+  #get 'posts/show'
+
+  #devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#end
+
+Rails.application.routes.draw do
+    root 'posts#index'
+    resources :posts
+    devise_for :users
+end
